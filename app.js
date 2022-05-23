fe9979937a0a10ae97ab40687942fef34e6e2b35
Nathan <nathanrouche30@gmail.com>
2022-05-23 16:13:57 +0000

    Base avec ExpressJS et MongoDB

---
+++ app.js
@@ -0,0 +1,28 @@
+// db: crud:UFKrghmMVorgGehN
+var express = require('express');
+var path = require('path');
+
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log('Connected to MongoDB');
+}
+
+//var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('layout', 'index');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', indexRouter);
+
+module.exports = app;
