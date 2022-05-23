9af913dc416b635260b2419288ceb53e6310eae9
Nathan <nathanrouche30@gmail.com>
2022-05-23 16:34:50 +0000

    CRUD Routes

--- controllers/users.js
+++ controllers/users.js
@@ -13,3 +13,24 @@ exports.read = function (req, res, next) {
         res.json(user);
     })
 }
+
+exports.create = function (req, res, next) {
+    User.create(req.body, (err, user) => {
+        if(err) return res.json(err);
+        res.json(user);
+    })
+}
+
+exports.delete = function (req, res, next) {
+    User.findByIdAndRemove(req.params.id, (err, user) => {
+        if(err) return res.json(err);
+        res.json(user);
+    })
+}
+
+exports.update = function (req, res, next) {
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, user) => {
+        if(err) return res.json(err);
+        res.json(user);
+    })
+}
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var controller = require("../controllers/users")
 
 router.get('/', controller.readAll);
+router.post('/', controller.create);
 router.get('/:id', controller.read);
+router.put('/:id', controller.update);
+router.delete('/:id', controller.delete);
 
 module.exports = router;
